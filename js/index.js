04f9a7200e52dbe54015de8bddb44232b01d16fc
Romainducos <ducosromain31@gmail.com>
2025-09-08 15:11:08 +0200

    feature/timer merge into develop

--- js/index.js
+++ js/index.js
@@ -1,3 +1,36 @@
+var timer = new easytimer.Timer();
+
+
+$('#countdown .countdown-value').html('02:00');
+
+var timerRunning = false;
+
+$('.startButton').click(function () {
+  if (!timerRunning) {
+    timer.start({countdown: true, startValues: {minutes : 2}});
+    timerRunning = true;
+    $(this).text('Reset');
+  } else {
+    timer.reset();
+    $('#countdown .countdown-value').html('02:00');
+    timer.pause();
+    timerRunning = false;
+    $(this).text('Start');
+  }
+});
+
+
+
+timer.addEventListener('secondsUpdated', function (e) {
+    $('#countdown .countdown-value').html(timer.getTimeValues().toString(['minutes', 'seconds']));
+});
+
+timer.addEventListener('targetAchieved', function (e) {
+    $('#countdown .countdown-value').html('GG');
+    timerRunning = false;
+    $('.startButton').text('Start');
+});
+
 const allTheBox = document.getElementsByTagName("button");
 
 const boxClicked = (e) => {
