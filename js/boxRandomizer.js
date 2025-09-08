136259d3d94bdfeae2b883ef321ce2d1d0ffa954
Lucas-132 <lucas.pollet135@gmail.com>
2025-09-08 15:34:18 +0200

    feat : added boxRandomizer function's javascript file

---
+++ js/boxRandomizer.js
@@ -0,0 +1,14 @@
+function randomizeBox(){
+    // Defining box's letters/numbers possibilities array and result variable
+    let letters = 'abcdefgh'
+    letters = letters.split('')
+    let numbers = '123456789'
+    numbers = numbers.split('')
+    let result = ''
+
+    // Generating the bow which the player will have to click on
+    result += letters[Math.floor(Math.random() * 8)]
+    result += numbers[Math.floor(Math.random() * 8)] //note: we're using a numbers array so as to exclude 0 from possibles values
+
+    return result;
+}
