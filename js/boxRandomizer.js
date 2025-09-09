458d35847979414bd2f37e6c2aa707581cf13ab4
LïamA <augu.liam@gmail.com>
2025-09-09 09:04:37 +0200

    variable fix

--- js/boxRandomizer.js
+++ js/boxRandomizer.js
@@ -1,19 +1,19 @@
-export const randomizedBox = document.getElementById("randomized-box")
-export let currentBox = a1
+export const randomizedBox = document.getElementById("randomized-box");
+export let currentBox = "a1";
 
-export function randomizeBox(){
-    // Defining box's letters/numbers possibilities array and result variable
-    let letters = 'abcdefgh'
-    letters = letters.split('')
-    let numbers = '123456789'
-    numbers = numbers.split('')
-    let result = ''
+export function randomizeBox() {
+  // Defining box's letters/numbers possibilities array and result variable
+  let letters = "abcdefgh";
+  letters = letters.split("");
+  let numbers = "123456789";
+  numbers = numbers.split("");
+  let result = "";
 
-    // Generating the bow which the player will have to click on
-    result += letters[Math.floor(Math.random() * 8)]
-    result += numbers[Math.floor(Math.random() * 8)] //note: we're using a numbers array so as to exclude 0 from possibles values
+  // Generating the bow which the player will have to click on
+  result += letters[Math.floor(Math.random() * 8)];
+  result += numbers[Math.floor(Math.random() * 8)]; //note: we're using a numbers array so as to exclude 0 from possibles values
 
-    randomizedBox.innerHTML = result
-    currentBox = result
-    return result;
+  randomizedBox.innerHTML = result;
+  currentBox = result;
+  return result;
 }
